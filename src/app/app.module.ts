cced4f2eb544b5c1280c798909cc8db016c5e260
Devin Lewis <dglewis@umich.edu>
2022-10-17 11:36:57 -0500

    updating files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 import { MessageModalComponent } from './aws/message-modal/message-modal.component';
 import { ExerciseMenuComponent } from './exercise-menu/exercise-menu.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { ExerciseInfoComponent } from './exercise-info/exercise-info.component';
 
 @NgModule({
